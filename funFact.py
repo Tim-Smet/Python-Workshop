343e4e3e0d72247df4d4ef713c36aab6c5a90f27
Tim-Smet <tim2smet@gmail.com>
2020-06-01 14:59:37 +0200

    Adding some files

---
+++ funFact.py
@@ -0,0 +1,16 @@
+#import this
+
+#from __future__ import braces
+
+# a, b = 7, 8
+# a, b = b, a
+# print(a, b)
+
+# slicing = []
+# for i in range(0, 10):
+#    slicing.append(i)
+# print(slicing)
+# reverse = slicing[::-1]
+# print(reverse)
+
+# import antigravity
