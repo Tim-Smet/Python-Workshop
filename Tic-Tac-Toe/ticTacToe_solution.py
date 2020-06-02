c13f9df827d1602f8b7fda43c05384f4e1365f8d
Tim-Smet <tim2smet@gmail.com>
2020-06-02 11:55:28 +0200

    Tic tac toe works! But the computer is still prety stupid. Will make him smarter

--- Tic-Tac-Toe/ticTacToe_solution.py
+++ Tic-Tac-Toe/ticTacToe_solution.py
@@ -15,7 +15,7 @@ class Board():
     def check_cell_empty(self, cell_no):
         #If string on given index is empty, this returns false
         return self.cells[cell_no]
-  
+
     def update_cell(self, cell_no, player):
         if not self.check_cell_empty(cell_no):
             self.cells[cell_no] = player
@@ -47,7 +47,7 @@ class Board():
         os.system("clear") 
 
 
-class Players():
+class Contestants():
     def __init__(self):
         self.player = ""
         self.computer = ""
@@ -62,38 +62,101 @@ class Players():
             self.computer = "X"
 
 
-
 #Game play
 board = Board()
 board.refresh_screen()
 print("Decide who is who. X can start.")
 
-obj = Players()
-obj.decide_sign()
-player = obj.player
-computer = obj.computer
+contestant = Contestants()
+contestant.decide_sign()
+player = contestant.player
+computer = contestant.computer
 
 print("Player is: %s\n" %(player))
 print("Computer is: %s\n" %(computer))
 if player == "X":
     print("Player can start")
+    print("\nLet the game begin!!!")
+    game_is_on = True
+    while game_is_on:
+        board.drawBoard()
+        player_move = int(input("Pick your move, 1-9: "))
+        
+        player_turn = True
+        while player_turn:
+            if not board.check_cell_empty(player_move):
+                board.update_cell(player_move, player)
+                break
+            else:
+                print("This spot is already taken") 
+        
+        if board.who_won():
+            if board.who_won() == player:
+                board.drawBoard()
+                print("Player has won.")
+            else:
+                board.drawBoard()
+                print("Computer has won")
+            game_is_on = False
+        else:
+            computer_turn = True
+            while computer_turn:
+                computer_move = random.randint(1, 10)
+                if not board.check_cell_empty(computer_move):
+                    board.update_cell(computer_move, computer)
+                    computer_turn = False
+        
+            if board.who_won():
+                if board.who_won() == computer:
+                    board.drawBoard()
+                    print("Computer has won.")
+                else:
+                    board.drawBoard()
+                    print("Player has won")
+                game_is_on = False
+        
+        
 else:
     print("Computer will take first turn.")
-print("\nLet the game begin!!!")
-game_is_on = True
-while game_is_on:
-    board.drawBoard()
-    player_move = int(input("Pick your move, 1-9: "))
-    board.update_cell(player_move, player) 
-    if board.who_won():
-        if board.who_won() == player:
-            board.drawBoard()
-            print("Player has won.")
+    print("\nLet the game begin!!!")
+    game_is_on = True
+    while game_is_on:
+        computer_turn = True
+        while computer_turn:
+            computer_move = random.randint(1, 10)
+            if not board.check_cell_empty(computer_move):
+                board.update_cell(computer_move, computer)
+                board.drawBoard()
+                computer_turn = False
+        
+        if board.who_won():
+            if board.who_won() == computer:
+                board.drawBoard()
+                print("Computer has won.")
+            else:
+                board.drawBoard()
+                print("Player has won")
+            game_is_on = False
         else:
-            board.drawBoard()
-            print("Computer has won")
-        game_is_on = False
-    else:
-        print("No winner yet.")
+            player_move = int(input("Pick your move, 1-9: "))
+            player_turn = True
+            while player_turn:
+                if not board.check_cell_empty(player_move):
+                    board.update_cell(player_move, player)
+                    break
+            else:
+                print("This spot is already taken")
+
+            if board.who_won():
+                if board.who_won() == player:
+                    board.drawBoard()
+                    print("Player has won.")
+                else:
+                    board.drawBoard()
+                    print("Computer has won")
+                game_is_on = False
+        
+        
+
 
 print("Thank you for playing")
